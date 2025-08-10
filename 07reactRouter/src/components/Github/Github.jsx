cdf78b0c23c100a35f97ece40908c2fc3a4a6550
Suraj Kumar <surajkumar_230309@aitpune.edu.in>
2025-08-10 20:41:12 +0530

    Day 6

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData();
+
+  //   const [data, setData] = useState([]);
+
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/17surajkumar")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+  return (
+    <div className="text-center m-4 p-2 bg-gray-700 text-white text-3xl">
+      Github Followers: {data.followers}
+      <img
+        className="m-5"
+        src={data.avatar_url}
+        alt="Git Picture"
+        width={300}
+      />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/17surajkumar");
+  return response.json();
+};
